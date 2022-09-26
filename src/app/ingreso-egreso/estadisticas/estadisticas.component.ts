9a2fef6e8c57061aaa2465d26e75daa39e114256
Alan Barrios <alan.barr195@gmail.com>
2022-09-26 18:06:14 +0200

    Primer commit template listo

---
+++ src/app/ingreso-egreso/estadisticas/estadisticas.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estadisticas',
+  templateUrl: './estadisticas.component.html',
+})
+export class EstadisticasComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingreso-egreso',
+  templateUrl: './ingreso-egreso.component.html',
+})
+export class IngresoEgresoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
